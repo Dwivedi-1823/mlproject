6a420e87289c152413a03aa4702874ced906d1fe
Piyush Kumar <pyushkumar999@outlook.com>
2026-01-21 20:43:16 +0530

    req changes

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def predict_datapoint():
     
 if __name__ == "__main__":
      port = int(os.environ.get("PORT", 5000))
-     app.run(host="0.0.0.0", debug=True)
+     app.run(host="0.0.0.0", port=port)
 
 
 
