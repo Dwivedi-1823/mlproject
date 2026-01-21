c343c9be555eac8c7d2f32304d9d2782f4d65bd4
Piyush Kumar <pyushkumar999@outlook.com>
2026-01-21 15:55:56 +0530

    Home page and predct point styling

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
     
 if __name__ == "__main__":
+     port = int(os.environ.get("PORT", 5000))
      app.run(host="0.0.0.0", debug=True)
 
 
